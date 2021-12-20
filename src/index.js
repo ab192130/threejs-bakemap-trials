d905e5c63fbe7de2495a9ddeb530030ddb117dea
ab192130 <5365476+ab192130@users.noreply.github.com>
2021-12-20 15:25:35 +0400

    minor changes.

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ loader.setDRACOLoader(draco);
 // Spector
 let SPECTOR = require("spectorjs");
 let spector = new SPECTOR.Spector();
-spector.displayUI();
+// spector.displayUI();
 
 init()
 createFloor();
@@ -34,9 +34,9 @@ function init() {
 
     // Camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.x = 0;
-    camera.position.y = 10;
-    camera.position.z = -10;
+    camera.position.x = 3;
+    camera.position.y = 4;
+    camera.position.z = -3;
 
     // Render
     renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
